f654fd0b59f8f6d8a04cca3428509646727200c7
TheCanibal <ziryadraenei@hotmail.fr>
2024-01-25 16:47:03 +0100

    Changement du nom du microservice de l'interface

--- microservice-interface/src/main/java/com/patientui/PatientuiApplication.java
+++ microservice-interface/src/main/java/com/patientui/PatientuiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterfaceApplication {
+public class PatientuiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InterfaceApplication.class, args);
+		SpringApplication.run(com.example.demo.PatientuiApplication.class, args);
 	}
 
 }
---
+++ microservice-patientui/src/main/java/com/patientui/PatientuiApplication.java
@@ -0,0 +1,13 @@
+package com.patientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientuiApplication.class, args);
+	}
+
+}
